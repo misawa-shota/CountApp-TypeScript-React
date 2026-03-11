81cad0a375690b25a1ff298b0b191de57624317a
misawa-shota <kaiyou0120@gmail.com>
2026-03-11 20:08:26 +0900

    main.tsの編集

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react"
+import ReactDOM from "react-dom/client"
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import App from "./App"
+
+import { ChakraProvider, defaultSystem }
+from "@chakra-ui/react"
+
+ReactDOM.createRoot(
+  document.getElementById("root")!
+).render(
+
+  <React.StrictMode>
+
+    <ChakraProvider value={defaultSystem}>
+
+      <App />
+
+    </ChakraProvider>
+
+  </React.StrictMode>
+
 )
