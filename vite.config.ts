35eda92dfd8acfbc3f95e32f172aac4328e62c2f
misawa-shota <kaiyou0120@gmail.com>
2026-03-12 12:06:35 +0900

    pwa化のためのコードを追加#17

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import { VitePWA } from "vite-plugin-pwa"
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Delivery Guilt",
+        short_name: "Delivery Guilt",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#1a202c",
+        theme_color: "#1a202c",
+
+        icons: [
+          {
+            src: "/icon-192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "/icon-512.png",
+            sizes: "512x512",
+            type: "image/png"
+          }
+        ]
+      }
+    })
+  ]
 })
